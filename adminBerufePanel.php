74d614ffdb43da8b64164339f61629d2b5878316
UltronOne <UltronOne@users.noreply.github.com>
2019-07-18 14:30:34 +0200

    fix

--- adminBerufePanel.php
+++ adminBerufePanel.php
@@ -165,9 +165,9 @@ if(!empty($_GET['token'])){
                  ?>
                                 </div>
                 <div class="mdl-card__actions mdl-card--border">
-                  <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                    Erstellen
-                  </a>
+                <form action="importData.php" method="post" enctype="multipart/form-data" id="generateToken">
+             
+              <input type="submit" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" name="generateToken" value="Erstellen">
                 </div>
               
               </div>
